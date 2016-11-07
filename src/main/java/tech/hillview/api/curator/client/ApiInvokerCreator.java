c9a242d4f7c79e3b8ed02c7dc2cb6990270735e9
Tommy Xu <xuyijun@gmail.com>
2016-11-08 01:15:09 +0800

    add license information to source-code

--- src/main/java/tech/hillview/api/curator/client/ApiInvokerCreator.java
+++ src/main/java/tech/hillview/api/curator/client/ApiInvokerCreator.java
@@ -1,3 +1,8 @@
+/**
+ *                                  Apache License
+ *                            Version 2.0, January 2004
+ *                         http://www.apache.org/licenses/
+ */
 package tech.hillview.api.curator.client;
 
 import org.apache.curator.x.discovery.ServiceInstance;
--- src/test/java/tech/hillview/api/curator/client/test/api/AccountInfo.java
+++ src/test/java/tech/hillview/api/curator/client/test/api/AccountInfo.java
@@ -1,3 +1,8 @@
+/**
+ *                                  Apache License
+ *                            Version 2.0, January 2004
+ *                         http://www.apache.org/licenses/
+ */
 package tech.hillview.api.curator.client.test.api;
 
 import java.math.BigDecimal;
